3e4c29b3988687ca72213ec0d80b0817fa4adc82
tonykang22 <tony.kang486@gmail.com>
2023-07-13 13:38:47 +0900

    #8 - Init Spring boot project
    - Spring web
    - Actuator
    - DevTools
    - Lombok

---
+++ src/main/java/com/tonykang22/ordinaryboard/OrdinaryBoardApplication.java
@@ -0,0 +1,13 @@
+package com.tonykang22.ordinaryboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdinaryBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdinaryBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tonykang22/ordinaryboard/OrdinaryBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonykang22.ordinaryboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdinaryBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
