186ca6e1bbc5bd56e171ccfa419086b9947ea159
tonykang22 <tony.kang486@gmail.com>
2023-07-15 02:42:49 +0900

    #1 - Add @RestResource on repository

--- src/main/java/com/tonykang22/ordinaryboard/repository/ArticleRepository.java
+++ src/main/java/com/tonykang22/ordinaryboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.tonykang22.ordinaryboard.repository;
 
 import com.tonykang22.ordinaryboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
