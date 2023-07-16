5fa3dd009a4d3bb875a80d654abb53573b9b5a8a
tonykang22 <tony.kang486@gmail.com>
2023-07-16 12:58:03 +0900

    #1 - Disable spring-data-rest test
    Since spring-data-rest is just library served, do not need to test it.
    (And it is heavy and have impact on the database since it is integration test.)

--- src/test/java/com/tonykang22/ordinaryboard/controller/DataRestTest.java
+++ src/test/java/com/tonykang22/ordinaryboard/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.tonykang22.ordinaryboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외시킴")
 @DisplayName("Data REST - API 테스트")
 @AutoConfigureMockMvc
 @SpringBootTest
