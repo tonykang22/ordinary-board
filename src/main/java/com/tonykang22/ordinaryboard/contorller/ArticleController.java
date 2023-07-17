32597324295a7db3997289337d1903c334165492
tonykang22 <tony.kang486@gmail.com>
2023-07-17 17:07:11 +0900

    #18 - Add view test for Articles
    Test for article view.

---
+++ src/main/java/com/tonykang22/ordinaryboard/contorller/ArticleController.java
@@ -0,0 +1,11 @@
+package com.tonykang22.ordinaryboard.contorller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+
+}
