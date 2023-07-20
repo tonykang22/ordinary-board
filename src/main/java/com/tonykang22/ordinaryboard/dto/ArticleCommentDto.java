f06b8924c453bf377a693e89dfa7c637dd5f632c
tonykang22 <tony.kang486@gmail.com>
2023-07-20 16:36:06 +0900

    #20 - Edit ArticleComment related class
    - UserAccount 정보 추가로 변경 내용 반영

---
+++ src/main/java/com/tonykang22/ordinaryboard/dto/ArticleCommentDto.java
@@ -0,0 +1,43 @@
+package com.tonykang22.ordinaryboard.dto;
+
+import com.tonykang22.ordinaryboard.domain.Article;
+import com.tonykang22.ordinaryboard.domain.ArticleComment;
+
+import java.time.LocalDateTime;
+
+public record ArticleCommentDto(
+        Long id,
+        Long articleId,
+        UserAccountDto userAccountDto,
+        String content,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+) {
+
+    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleCommentDto(id, articleId, userAccountDto, content, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static ArticleCommentDto from(ArticleComment entity) {
+        return new ArticleCommentDto(
+                entity.getId(),
+                entity.getArticle().getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getContent(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public ArticleComment toEntity(Article entity) {
+        return ArticleComment.of(
+                userAccountDto.toEntity(),
+                entity,
+                content
+        );
+    }
+}
