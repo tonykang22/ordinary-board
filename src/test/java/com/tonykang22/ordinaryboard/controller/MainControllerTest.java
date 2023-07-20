1dcd68a57a8e5380adff9e2f4e90dfa16a5fdecb
tonykang22 <tony.kang486@gmail.com>
2023-07-20 16:37:02 +0900

    #20 - Add main controller
    - When try to get root page, redirect to /articles

---
+++ src/test/java/com/tonykang22/ordinaryboard/controller/MainControllerTest.java
@@ -0,0 +1,38 @@
+package com.tonykang22.ordinaryboard.controller;
+
+import com.tonykang22.ordinaryboard.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("View 컨트롤러 - Root")
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+
+    private final MockMvc mvc;
+
+    MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    void given_whenRequestingRootPage_thenRedirectToArticlesPage() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/articles"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+}
