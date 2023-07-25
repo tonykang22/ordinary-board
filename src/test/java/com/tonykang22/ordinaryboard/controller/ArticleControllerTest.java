f4245489a35f48bad905034cbb03db874ffe9e19
tonykang22 <tony.kang486@gmail.com>
2023-07-25 17:08:21 +0900

    #29 - Send SearchTypes when use search
    To delete dependencies from Front-Back code.

--- src/test/java/com/tonykang22/ordinaryboard/controller/ArticleControllerTest.java
+++ src/test/java/com/tonykang22/ordinaryboard/controller/ArticleControllerTest.java
@@ -1,6 +1,7 @@
 package com.tonykang22.ordinaryboard.controller;
 
 import com.tonykang22.ordinaryboard.config.SecurityConfig;
+import com.tonykang22.ordinaryboard.domain.type.SearchType;
 import com.tonykang22.ordinaryboard.dto.ArticleWithCommentsDto;
 import com.tonykang22.ordinaryboard.dto.UserAccountDto;
 import com.tonykang22.ordinaryboard.service.ArticleService;
@@ -36,8 +37,10 @@ class ArticleControllerTest {
 
     private final MockMvc mvc;
 
-    @MockBean ArticleService articleService;
-    @MockBean PaginationService paginationService;
+    @MockBean
+    ArticleService articleService;
+    @MockBean
+    PaginationService paginationService;
 
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -59,6 +62,31 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"))
                 .andExpect(model().attributeExists("paginationBarNumbers"));
         then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+
+    @DisplayName("[view]: GET - 게시글 리스트 (게시판) 페이지: 검색어와 호출")
+    @Test
+    void givenNothing_whenSearchingArticlesView_thenReturnsArticlesView() throws Exception {
+        // given
+        SearchType searchType = SearchType.TITLE;
+        String searchValue = "title";
+        given(articleService.searchArticles(eq(searchType), eq(searchValue), any(Pageable.class)))
+                .willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
+
+        // when & then
+        mvc.perform(
+                get("/articles")
+                        .queryParam("searchType", String.valueOf(searchType))
+                        .queryParam("searchValue", searchValue)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("paginationBarNumbers"));
+        then(articleService).should().searchArticles(eq(searchType), eq(searchValue), any(Pageable.class));
         then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
     }
 
